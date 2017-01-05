cd46602acf1bc542c82aa4ed8d59b022840dc2c0
Justin Karneges <justin@karneges.com>
2017-01-05 11:27:57 -0800

    load settings from urls with defaults

--- server/settings.py
+++ server/settings.py
@@ -74,12 +74,10 @@ WSGI_APPLICATION = 'server.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.10/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
+DATABASES = {}
+
+import dj_database_url
+DATABASES['default'] = dj_database_url.config(default='sqlite:///db.sqlite3')
 
 
 # Password validation
@@ -123,4 +121,7 @@ STATIC_ROOT = 'staticfiles'
 
 from gripcontrol import parse_grip_uri
 
-GRIP_PROXIES = [parse_grip_uri(os.environ['GRIP_URL'])]
+if 'GRIP_URL' in os.environ:
+	GRIP_PROXIES = [parse_grip_uri(os.environ['GRIP_URL'])]
+else:
+	GRIP_PROXIES = []
